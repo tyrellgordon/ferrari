56bac644c78fa80584275206166b41e24ea062ae
doctorpillcosby <gordontyrell1993@gmail.com>
2018-11-13 02:52:59 -0500

    readded video asset and redid css stylization

---
+++ ferrari-website/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Video from './Video';
+import ReactPlayer from 'react-player'
+
+class App extends Component {
+
+  
+       
+  render() {
+    return (
+      <div className="App">
+       
+     <h1 class="victory">FERRARiROCHER</h1>
+     <Video/>
+ 
+        
+
+         
+  
+  
+       
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ferrari-website/src/Video.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactPlayer from 'react-player'
+
+class Video extends Component {
+
+  
+       
+  render() {
+    return (
+      <div >
+     
+          
+
+
+      </div>
+    );
+  }
+}
+
+export default Video;

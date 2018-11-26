b17a455e24d2f8e13c2cf14835e866840045f190
doctorpillcosby <gordontyrell1993@gmail.com>
2018-11-26 02:58:24 -0500

    production build set, hopefully it'll delploy now

---
+++ ferrari-website/build/precache-manifest.0fd76c1eb1834b22efb0c86467c534d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8602e1bb9aaf434859f30c53e4ba18e1",
+    "url": "/react-gh-pages/static/media/cruiserfortressengraveital.8602e1bb.ttf"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "4038f007b14f7e90891c",
+    "url": "/react-gh-pages/static/js/main.4038f007.chunk.js"
+  },
+  {
+    "revision": "cb770a44003e86b1d74e",
+    "url": "/react-gh-pages/static/js/1.cb770a44.chunk.js"
+  },
+  {
+    "revision": "4038f007b14f7e90891c",
+    "url": "/react-gh-pages/static/css/main.8854aeab.chunk.css"
+  },
+  {
+    "revision": "6710b3cc576e4e57d992e23edf08c28b",
+    "url": "/react-gh-pages/index.html"
+  }
+];
